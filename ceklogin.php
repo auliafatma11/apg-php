859946d819170feb720295f60fd5c49c58560234
Aulia Fatmawati <aulia.fatmawati196@smk.belajar.id>
2025-01-22 11:59:26 +0700

    APG-PHP

--- ceklogin.php
+++ ceklogin.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include "config.php";
+    include "library/config.php";
 
     $username = $_POST['username'];
     $password = $_POST['password'];
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     session_start();
     ob_start();
 
-    include "config.php";
+    include "library/config.php";
 
     if (empty($_SESSION['username']) OR empty($_SESSION['password'])) {
         echo "<p align='center'>Anda harus login terlebih dahulu!</p>";
